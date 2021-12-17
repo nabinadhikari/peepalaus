cd493761a6fce4846da081b77cbc2c5a495aec53
Nabin Adhikari <mr.nabinadhikari@gmail.com>
2021-12-17 23:45:02 +1100

    noscript gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-      const initialProps = await Document.getInitialProps(ctx)
-      return { ...initialProps }
-    }
-  
-    render() {
-      return (
-        <Html>
-          <Head />
-          <body>
-          <noscript dangerouslySetInnerHTML={{__html:`
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M68NPWP"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `}} />
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      )
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
-  
-  export default MyDocument
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-M68NPWP"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
